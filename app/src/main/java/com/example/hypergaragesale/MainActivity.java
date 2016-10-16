1a2098963e6cf265c98f56855910e999e2613e1d
Michael L. Mehr <mike@azuresults.com>
2016-10-16 12:56:08 -0700

    Ex.2 Add Menu Item for Post.
    Third and final section of Steps #2.

--- app/src/main/java/com/example/hypergaragesale/MainActivity.java
+++ app/src/main/java/com/example/hypergaragesale/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 // This is called NewPostActivity in the canonical project
@@ -23,9 +25,28 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    public void newItemAdded(View v) {
+    private void showSnackBar() {
         Snackbar.make(findViewById(R.id.myCoordinatorLayout), R.string.new_post_snackbar,
                 Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void newItemAdded(View v) {
+        showSnackBar();
         System.out.println("Pressed Post - NewItem button");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.new_post_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_new_post) {
+            showSnackBar();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
