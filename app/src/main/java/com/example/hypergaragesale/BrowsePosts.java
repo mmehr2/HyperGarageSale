d0c63f8e2adf95aa8ac3867e1f439b2c6a9dfb36
Michael L. Mehr <mike@azuresults.com>
2016-10-19 22:13:18 -0700

    Ex.4.1. Added RecyclerView to BrowsePosts activity. NONWORKING
    Code does not implement database yet (coming in 4.2).

---
+++ app/src/main/java/com/example/hypergaragesale/BrowsePosts.java
@@ -0,0 +1,16 @@
+package com.example.hypergaragesale;
+
+public class BrowsePosts {
+    String mTitle;
+    String mPrice;
+
+    public BrowsePosts() {
+        mTitle = "";
+        mPrice = "0.00";
+    }
+
+    public BrowsePosts(String title, String price) {
+        mTitle = title;
+        mPrice = price;
+    }
+}
--- app/src/main/java/com/example/hypergaragesale/BrowsePostsActivity.java
+++ app/src/main/java/com/example/hypergaragesale/BrowsePostsActivity.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class BrowsePostsActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_posts);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -27,6 +37,21 @@ public class BrowsePostsActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.posts_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        ArrayList<BrowsePosts> myDataSet = new ArrayList<>(); // TODO: eliminate fake data
+        mAdapter = new PostsAdapter(myDataSet); //getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 }
